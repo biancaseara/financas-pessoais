95d5815d2c346bd865675f85cf8b203a63147f01
Bianca <biancaseara.p@exemplo.com>
2026-04-21 12:59:33 -0300

    chore: refine gitignore and implement .env.example

--- config/database.php
+++ config/database.php
@@ -20,9 +20,9 @@ class Database {
         try {
             $dsn = "mysql:host=" . $this->host . ";port=" . $this->port . ";dbname=" . $this->dbname . ";charset=utf8mb4";
             $this->pdo = new PDO($dsn, $this->user, $this->password);
-            // Configuração do PDO para lançar exceções em caso de erro
+           
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            // Retorna dados como array associativo por padrão
+            
             $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         } catch(PDOException $e) {
             die("Erro de Conexão: " . $e->getMessage());
